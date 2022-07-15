92aac95a954a63cedb98b3d8992eb2314b846995
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-15 13:43:04 +0100

    Add first 3 examples to repo

---
+++ ex2.py
@@ -0,0 +1,8 @@
+# A comment. This is so you can read your program later.
+# Anything after the # is ignored by python.
+print("I could have code like this.") # and the comment after is ignored.
+
+# You can also use a comment to 'disable' or comment out code:
+# print("This won't run.")
+print("This will run!")
+print("Hi, my # is 08036095486")

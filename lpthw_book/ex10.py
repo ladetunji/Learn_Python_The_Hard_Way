8fa6163c04e3e2d76cfcd210cfa2106cef5324b5
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-18 19:26:09 +0100

    Add example 10

---
+++ lpthw_book/ex10.py
@@ -0,0 +1,17 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a new line."
+backslash_cat = "I'm \\ a \\ cat."
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Cat nip\n\t* Grass
+"""
+website = """You can visit \a
+https:\/\/www.brandify.ng\/\rfor more details."""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+print(website)

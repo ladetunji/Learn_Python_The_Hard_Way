bfc7e6851361058c77ad1e876ce7d75832d027e5
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-21 12:41:36 +0100

    Add exercise 10

--- lpthw_book/ex10.py
+++ lpthw_book/ex10.py
@@ -1,17 +1,31 @@
 tabby_cat = "\tI'm tabbed in."
+aced_cat = "\aI'm aced in."
+racer_cat = "I'm a racer \rcat sir."
 persian_cat = "I'm split\non a new line."
-backslash_cat = "I'm \\ a \\ cat."
+backslash_cat = "I'm \\ a \\ cat \\ dog."
+backspace_cat = "I am a  \bbreakdancing \fcat."
 fat_cat = """
 I'll do a list:
 \t* Cat food
 \t* Fishies
-\t* Cat nip\n\t* Grass
+\t* Cat nip
+\t* Grass
 """
-website = """You can visit \a
-https:\/\/www.brandify.ng\/\rfor more details."""
+slim_cat = '''Which works best for you?
+a. {} or
+b. {} or
+c. {}
+'''.format("morning", "afternoon", "evening")
+quote_cat = "How do I say \"what I imply\" without saying it?"
+website = """You can visit
+https://www.brandify.ng/ for more details."""
 
 print(tabby_cat)
+print(aced_cat)
+print(racer_cat)
 print(persian_cat)
 print(backslash_cat)
+print(backspace_cat)
 print(fat_cat)
-print(website)
+print(slim_cat)
+print(quote_cat)

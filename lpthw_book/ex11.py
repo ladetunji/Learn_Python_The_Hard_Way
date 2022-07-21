0538af930c918d44b8a9408d33611e5da0535437
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-21 13:40:14 +0100

    Add exercise 11

---
+++ lpthw_book/ex11.py
@@ -0,0 +1,10 @@
+print("Do you want to take this quiz?\nAnswer Yes (y) or No (n)")
+answer = input()
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you?", end = ' ')
+height = input()
+print("How much do you weigh?", end = ' ')
+weight = input()
+print(f"""You responded {answer} to the quiz.
+So you are {age} years old, {height} tall and {weight} heavy.""")

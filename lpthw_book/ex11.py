13984310793b4310519317160e91e15dc4119ae0
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-23 20:49:19 +0100

    Update input() function in exercise 11

--- lpthw_book/ex11.py
+++ lpthw_book/ex11.py
@@ -1,10 +1,13 @@
 print("Do you want to take this quiz?\nAnswer Yes (y) or No (n)")
 answer = input()
+name = input("What is your name? ")
 print("How old are you?", end = ' ')
 age = input()
 print("How tall are you?", end = ' ')
 height = input()
 print("How much do you weigh?", end = ' ')
 weight = input()
-print(f"""You responded {answer} to the quiz.
-So you are {age} years old, {height} tall and {weight} heavy.""")
+print(f"""
+You responded \"{answer}\" to the quiz.
+Your name is {name}, you are {age} years old, {height} tall and {weight} heavy.
+""")

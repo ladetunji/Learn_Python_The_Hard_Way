a48127ab229a6168e9a3fb0df5dabd48afc611d6
ladetunji <ladetunji.osibanjo@gmail.com>
2022-07-23 21:27:44 +0100

    Add exercise 12

---
+++ lpthw_book/ex12.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"Your name is {name}, you are {age} years old, you are {height}cm tall and you weigh {weight}kg.")
